26c3a20c021911ba9f0943f6febdaf6a110f5c5d
blandinisabella23-a11y <blandin.isabella23@gmail.com>
2025-12-06 11:56:34 -0400

    contador

--- src/componentes/Counter.tsx
+++ src/componentes/Counter.tsx
@@ -1,15 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function Counter() {
     const [count, setCount] = useState(0);
+    const [esPar, setEsPar] = useState(true);
 
+    useEffect (() => {
+        if (count % 2 === 0) {
+            setEsPar(true);
+        } else {
+            setEsPar(false);
+        }
+    }, [count]);
     return (
-        <div className="w-screen text-[#FCFCFD] bg-[#3843D0] h-screen flex flex-col justify-center items-center py-10 px-4 ">
+        <div className={`w-screen text-[#FCFCFD]
+            esParm ? "bg-orange-500" "bg-[#3843D0]" 
+        } h-screen flex flex-col justify-center items-center py-10 px-4`}
+        >
             <h1 className=" text-3xl mb-6 font-semibold"> Contador</h1>
+
             <p className="text-6xl font-bold">{count}</p>
+
             <button
             onClick={() => setCount(count+1)}
-            className="mt-8 px-6 py2 text-[3843D0] border-2 bg-white font-medium rounded-lg hover:bg-[#FCFCFD] transition"
+            className={`mt-8 px-6 py2 ${
+                esPar ? "text-orange-500" : "text-[3843D0]"
+             } border-2 bg-white font-medium rounded-lg hover:bg-[#FCFCFD] transition"`}
             >
                 Sumar + 1
             </button>
